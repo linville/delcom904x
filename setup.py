c29b173a5316e2e02e51bd2859d138dd49cf3885
Aaron Linville <aaron@linville.org>
2019-09-05 22:50:45 -0400

    Add long description for PyPi.

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="delcom904x",
-    version="0.2",
+    version="0.2.1",
     description="A python class to control Delcom USBLMP Products 904x multi-color, USB, visual signal indicators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Aaron Linville",
     author_email="aaron@linville.org",
     url="https://github.com/linville/delcom904x",
